ac550ec2c61e7095b94de6bd53f90c6da8ca3067
Andrew Kennel <akennel@gmail.com>
2020-02-23 10:18:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'SharePoint Development', // Required
+  author: 'Andrew Kennel', // Required
+  description: 'Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://akennel.com',
+    github: 'https://github.com/akennel',
+    linkedin: 'https://www.linkedin.com/in/andrew-kennel-b154893/',
+    train: 'https://train.akennel.com'
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://akennel.com',
 };
