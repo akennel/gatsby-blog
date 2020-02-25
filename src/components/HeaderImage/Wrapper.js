7192c5ba05ba31449c43fa2caf5cdfdaea081652
Andrew Kennel <akennel@berkadia.com>
2020-02-25 15:46:15 -0500

    changed logo image

--- src/components/HeaderImage/Wrapper.js
+++ src/components/HeaderImage/Wrapper.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   border: 5px solid #3598db;
+  background-color: #3598db;
   border-radius: 50%;
-  height: 150px;
   margin: 0 auto 15px auto;
   overflow: hidden;
   width: 150px;
@@ -11,6 +11,7 @@ const Wrapper = styled.div`
   img {
     padding: 5px;
     border-radius: 50%;
+    margin-top: 8px;
   }
 `;
 
